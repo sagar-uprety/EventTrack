9bb1da937dbcc60fe41dc2159b7598ad3d9cd510
Sagar Uprety <sagarupreti100@gmail.com>
2019-12-12 11:01:19 +0545

    Added ejs files and styles-navbar

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//initialization start
+//initialization starts
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
@@ -6,13 +6,20 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs"); 
+
 //initialization ends 
+
 app.get("/", function(req, res) {
-  res.send("Home Page Reached");
+  res.render("index");
+});
+
+
+app.get("/events", function(req, res) {
+  res.render("events");
 });
 
 app.get("*", function(req, res) {
-  res.send("<h1>Wallah</h1>"); 
+  res.send("Uh Oh!!! I guess you are a bit lost!!!"); 
 });
 
 var port = process.env.PORT || 3000;
