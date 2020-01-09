dad9b7c21212866f501eda367e21f02dc4fff4f9
Sagar Uprety <sagarupreti100@gmail.com>
2020-01-09 16:29:35 +0545

    comment model added

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 //initialization starts
 
-const express = require("express"),
+const express = require('express'),
       app = express(),
-      bodyParser = require("body-parser"),
-      mongoose = require("mongoose"),
-      Event = require("./models/events"),
-      seedDB = require("./seeds");
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      Event = require('./models/events'),
+      Comment= require('./models/comments'),
+      seedDB = require('./seeds')
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
-mongoose.connect("mongodb://localhost/EventTrack");
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.connect('mongodb://localhost/EventTrack')
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-seedDB();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+seedDB()
 
 //initialization ends
-
 
 //addind data to database
 
@@ -51,61 +51,98 @@ seedDB();
   }
 });  */
 
-app.get("/", function(req, res) {
-  res.render("index");
-});
+app.get('/', function (req, res) {
+  res.render("index")
+})
 
-app.get("/events", function(req, res) {
-  Event.find({}, function(err, allEvents) {
+app.get('/events', function (req, res) {
+  Event.find({}, function (err, allEvents) {
     if (err) {
-      console.log(err);
+      console.log(err)
     } else {
-      res.render("events", { events: allEvents });
+      res.render("Events/events", { events: allEvents })
     }
-  });
-});
+  })
+})
 
 //Create a new event and add to the Database
-app.post("/events", function(req, res) {
-  var Name = req.body.eventName;
-  var Venue = req.body.eventVenue;
-  var URL = req.body.eventURL;
-  var Description= req.body.eventDescription;
+app.post('/events', function (req, res) {
+  var Name = req.body.eventName
+  var Venue = req.body.eventVenue
+  var URL = req.body.eventURL
+  var Description = req.body.eventDescription
   var newEvent = {
     name: Name,
     venue: Venue,
     image: URL,
     description: Description
-  };
-  Event.create(newEvent, function(err, newlyCreated) {
+  }
+  Event.create(newEvent, function (err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
-      res.redirect("/events");
+      res.redirect("Events/events");
+    }
+  })
+})
+
+app.get('/events/new', function (req, res) {
+  res.render("Events/newEvent")
+})
+
+app.get('/events/:id', function (req, res) {
+  Event.findById(req.params.id)
+    .populate('comments')
+    .exec(function (err, foundEvent) {
+      if (err) {
+        console.log(err)
+      } else {
+        //render show template with that event
+        res.render("Events/show", { events: foundEvent })
+      }
+    })
+})
+
+//Comment Routes
+
+app.get("/events/:id/comments/new", function(req, res) {
+  // find campground by id
+  Event.findById(req.params.id, function(err, events) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("Comments/new", { events: events });
     }
   });
 });
 
-app.get("/events/new", function(req, res) {
-  res.render("newEvent");
-});
-
-app.get("/events/:id",function(req,res){
-  Event.findById(req.params.id, function(err, foundEvent){
-        if(err){
-            console.log(err);
+app.post("/events/:id/comments", function(req, res) {
+  //lookup campground using ID
+  Event.findById(req.params.id, function(err, events) {
+    if (err) {
+      console.log(err);
+      res.redirect("Events/events");
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
         } else {
-          //render show template with that campground
-            res.render("show", {events: foundEvent});
+          events.comments.push(comment);
+          events.save();
+          res.redirect("/events/" + events._id);
         }
+      });
+    }
   });
+  //create new comment
+  //connect new comment to campground
+  //redirect campground show page
+});
+app.get('*', function (req, res) {
+  res.send('Uh Oh!!! I guess you are a bit lost!!!')
 })
 
-app.get("*", function(req, res) {
-  res.send("Uh Oh!!! I guess you are a bit lost!!!");
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Server Has Started!");
-});
+var port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log('Server Has Started!')
+})
